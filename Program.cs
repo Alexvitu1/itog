397835561205563d26350bc93672e011cd3c8df0
alex <alexvitu@yandex.ru>
2023-05-08 14:13:00 +0300

    создание массива и наполнение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Clear();
+int SetNumber(string text = "")
+{
+     Console.Write($"Введите размер массива {text}:");
+     int num = Convert.ToInt32(Console.ReadLine());
+     return num;
+}
+
+void MetodArray(string[] array)
+{
+    int length = array.Length;
+    Random rnd = new Random();
+    for(int i=0; i<length; ++i)
+    {
+       Console.Write($"введите {i} элемент массива: ");
+       array[i] = Console.ReadLine();
+    }
+}
+void Print(string[] array)
+{
+    for(int i=0; i<array.Length; ++i )
+    {
+        Console.Write($" {array[i]}");
+    }
+}
+
+
+
+int length = SetNumber();
+string[] array = new string[length];
+MetodArray(array);
+Print(array);
